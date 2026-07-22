e2450ab563678b80075a9cad77a33fa02148425a
Aniruddh Vishwakarma <vishwaaniruddh@gmail.com>
2026-07-22 16:40:48 +0530

    Fix CORS Allow-Credentials header condition

--- api/cors_header.php
+++ api/cors_header.php
@@ -1,21 +1,19 @@
 <?php
 // admin/api/cors_header.php
 
-if (function_exists('header_remove')) {
-    header_remove('Access-Control-Allow-Origin');
-    header_remove('Access-Control-Allow-Credentials');
-    header_remove('Access-Control-Allow-Methods');
-    header_remove('Access-Control-Allow-Headers');
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+
+if (!empty($origin)) {
+    header("Access-Control-Allow-Origin: " . $origin);
+    header("Access-Control-Allow-Credentials: true");
+} else {
+    header("Access-Control-Allow-Origin: *");
 }
 
-$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
-
-header("Access-Control-Allow-Origin: " . $origin);
-header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Session-Token");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit(0);
 }
