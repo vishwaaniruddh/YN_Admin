45a02062c9f098f1526f0599fa75034cbdccf114
Aniruddh Vishwakarma <vishwaaniruddh@gmail.com>
2026-07-22 16:36:36 +0530

    Fix duplicate CORS headers in Apache and PHP

--- api/cors_header.php
+++ api/cors_header.php
@@ -1,26 +1,23 @@
 <?php
 // admin/api/cors_header.php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Max-Age: 86400");
-} else {
-    header("Access-Control-Allow-Origin: *");
+
+if (function_exists('header_remove')) {
+    header_remove('Access-Control-Allow-Origin');
+    header_remove('Access-Control-Allow-Credentials');
+    header_remove('Access-Control-Allow-Methods');
+    header_remove('Access-Control-Allow-Headers');
 }
 
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
+
+header("Access-Control-Allow-Origin: " . $origin);
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Session-Token");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    }
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-        header("Access-Control-Allow-Headers: " . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
-    } else {
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Session-Token");
-    }
     http_response_code(200);
     exit(0);
 }
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Session-Token");
